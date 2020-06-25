bc058ba21cdd3cae00349c2f2cab7e182967224b
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-06-25 14:41:58 -0400

    Added functions for handleing changes and submits to get ready for next step of getting the users data. Adjusted styling too.

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -17,14 +17,21 @@ class Contact extends React.Component {
     }
 
     handleChange = (event) => {
-        console.log(event);
 
-        const target = event.target
+        const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
-
+        
         this.setState({
             [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            disabled: true
         })
     }
 
@@ -36,25 +43,25 @@ class Contact extends React.Component {
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Group className="formGroup">
                         <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                        <Form.Control className="fillable" id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+                        <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
                     </Form.Group>
                     
                     <Form.Group className="formGroup">
                         <Form.Label htmlFor="email">Email</Form.Label>
-                        <Form.Control className="fillable" id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+                        <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
                     </Form.Group>
 
                     <Form.Group className="formGroup">
                         <Form.Label htmlFor="message">Message</Form.Label>
-                        <Form.Control className="fillable" id="message" name="message" as="textarea" rows="4" value={this.state.message} onChange={this.handleChange} />
+                        <Form.Control id="message" name="message" as="textarea" rows="4" value={this.state.message} onChange={this.handleChange} />
                     </Form.Group>
 
-                    <Button className="d-inline-block" variant="primary" type="submit" disables={this.state.disabled}>
+                    <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
                         Send
                     </Button>
 
-                    {this.state.emailSent === true && <p className="d-inline success-message">Email Sent</p>}
-                    {this.state.emailSent === false && <p className="d-inline error-message">Email Not Sent</p>}
+                    {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                    {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
                 </Form>
             </div>
         )
